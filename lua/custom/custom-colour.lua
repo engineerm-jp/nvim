f64c08545fe39cc152b2f0f37bc4f9400f382ba4
Masahiro Kobayashi <75647798+engineerm-jp@users.noreply.github.com>
2024-05-20 10:45:29 +1200

    change visual mode select colour

--- lua/custom/custom-colour.lua
+++ lua/custom/custom-colour.lua
@@ -3,11 +3,11 @@ vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', {fg = "DarkRed"})
 vim.api.nvim_set_hl(0, 'Comment', {fg = "#64BC00"})
 -- vim.api.nvim_set_hl(0, 'GitSignsChange', {fg = "DarkRed", bg="#280000"})
 vim.api.nvim_set_hl(0, 'GitSignsChange', {fg = "DarkRed"})
-
+-- vim.api.nvim_set_hl(0, 'GitSignsChangeLn', {bg="#031C00"})
 -- set background to none
 vim.api.nvim_set_hl(0, "normal", {guibg=NONE})
 vim.api.nvim_set_hl(0, "linenr", {fg = '#a0a8cd'})
-vim.api.nvim_set_hl(0, "visual", {guibg=NONE, fg="grey"})
+vim.api.nvim_set_hl(0, "visual", {bg="#414190"})
 
 vim.api.nvim_set_hl(0, "Search", {bg = '#38a89d', fg = 'Black'})
 vim.api.nvim_set_hl(0, "CurSearch", {bg = '#474853', fg = reverse})
